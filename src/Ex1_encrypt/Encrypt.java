c3870fa15b331bc899f823cb92d7003ab0059b39
MfDevNet <MfDevNet@gmail.com>
2019-03-12 23:07:24 +0330

    add main method get message and key from user

--- src/Ex1_encrypt/Encrypt.java
+++ src/Ex1_encrypt/Encrypt.java
@@ -1,4 +1,18 @@
 package Ex1_encrypt;
 
+import java.util.Scanner;
+
 public class Encrypt {
+    public static void main(String[] args) {
+        // get message and key from user
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("This Program Encrypt Message ");
+        System.out.print("Enter Message : ");
+        String message = scanner.nextLine();
+        System.out.print("Enter Key : ");
+        int key = scanner.nextInt();
+
+
+    }
+
 }

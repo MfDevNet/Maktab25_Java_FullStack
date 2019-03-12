333692151b4522e0671775c9d21652ff653de4cc
MfDevNet <MfDevNet@gmail.com>
2019-03-13 01:11:27 +0330

    Implement main method declare variable and codition

--- src/Ex3_Billing/Billing.java
+++ src/Ex3_Billing/Billing.java
@@ -2,6 +2,28 @@ package Ex3_Billing;
 
 public class Billing {
 
+    public static void main(String[] args) {
+        //declare variable
+        int photoBookPrice = 100;
+        int photoBookQuantity = 5;
+        int photoBookCoupon = 20;
+
+        // if price is valid  and quantity Coupon is zero  call computeBill by 1 parameter
+        System.out.println("Call method one parameter(Price ony)");
+        System.out.println("photoBook Price = " + computeBill(photoBookPrice));
+
+
+        // if price  and quantity is valid  Coupon is zero  call computeBill by 2 parameter
+
+        System.out.println("Call method tow parameter(Price and Quantity)");
+        System.out.println("photoBook Price = " + computeBill(photoBookPrice, photoBookQuantity));
+
+        // if price  and quantity and coupon is valid call computeBill by 3 parameter
+        System.out.println("Call method three parameter(Price and Quantity and Coupon)");
+        System.out.println("photoBook Price = " + computeBill(photoBookPrice, photoBookQuantity, photoBookCoupon));
+
+    }
+
     // static method computeBill overloaded
 
     static int computeBill(int price) {

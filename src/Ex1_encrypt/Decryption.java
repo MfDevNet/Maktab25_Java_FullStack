e0c416bd0cb22b5e250c3e37b03e1b67ef6e441a
MfDevNet <MfDevNet@gmail.com>
2019-03-12 23:21:19 +0330

    declare variable declare getCode Method declare getMessage method declare deCrypt method declare deCryptAll method

--- src/Ex1_encrypt/Decryption.java
+++ src/Ex1_encrypt/Decryption.java
@@ -1,4 +1,37 @@
 package Ex1_encrypt;
 
 public class Decryption {
+    // declare variable
+    private String message;
+    private int key;
+
+    // declare getCode Method //get code and set this key
+    public void getCode(int key) {
+        this.key = key;
+    }
+
+    //declare getMessage method // get message and set this message
+    public void getMessage(String message) {
+        this.message = message;
+    }
+
+    //declare deCrypt method
+    //change input char  and  return new char
+    private char deCrypt(char character) {
+        return (char) ((int) character - key);
+    }
+
+    //declare deCryptAll method
+    //change message char by char and no change space // back to original message
+    public String deCryptAll() {
+        String temp = "";
+        for (int i = 0; i < message.length(); i++) {
+            if (message.charAt(i) == ' ') {
+                temp += message.charAt(i);
+            } else
+                temp += deCrypt(message.charAt(i));
+
+        }
+        return temp;
+    }
 }

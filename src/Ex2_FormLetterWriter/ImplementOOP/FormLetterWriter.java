6961e6f0c012654c9a2d9cfc52cceb8ca3ce6c79
MfDevNet <MfDevNet@gmail.com>
2019-03-13 00:00:07 +0330

    creates main method get info from user create object and call displaySalutation

--- src/Ex2_FormLetterWriter/ImplementOOP/FormLetterWriter.java
+++ src/Ex2_FormLetterWriter/ImplementOOP/FormLetterWriter.java
@@ -1,5 +1,21 @@
 package Ex2_FormLetterWriter.ImplementOOP;
 
+import java.util.Scanner;
+
 public class FormLetterWriter {
+    public static void main(String[] args) {
+
+    //get info from user
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter First Name : ");
+        String fName=scanner.nextLine();
+        System.out.print("Enter Last Name : ");
+        String lName=scanner.nextLine();
+        //create object and call displaySalutation
+        displaySalutation dS=new displaySalutation();
+        dS.displaySalutation(fName,lName);
+
+
+    }
 
 }

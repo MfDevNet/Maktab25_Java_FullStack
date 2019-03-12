9877002dcdf163fac0ba8ba7d14664b1f3ef1764
MfDevNet <MfDevNet@gmail.com>
2019-03-13 01:51:01 +0330

    creates class BloodData and declare variable constructor no parameter

---
+++ src/Ex4_TestBloodData/BloodData.java
@@ -0,0 +1,16 @@
+package Ex4_TestBloodData;
+
+public class BloodData {
+
+    // declare variable
+    private String bloodType = null;
+    private String rhFactor = null;
+
+    //constructor without parameter
+    public BloodData() {
+        this.bloodType = "O";
+        this.rhFactor = "+";
+    }
+
+
+}

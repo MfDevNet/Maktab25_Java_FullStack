34972e394d704866d5ca35dc21f58f3a3599ad9f
MfDevNet <MfDevNet@gmail.com>
2019-03-13 01:09:28 +0330

    Implement method computeBill overloaded 3 parameter

--- src/Ex3_Billing/Billing.java
+++ src/Ex3_Billing/Billing.java
@@ -2,4 +2,17 @@ package Ex3_Billing;
 
 public class Billing {
 
+    // static method computeBill overloaded
+
+    static int computeBill(int price) {
+        return (price * 8 / 100) + price;
+    }
+
+    static int computeBill(int price, int quantity) {
+        return computeBill(price) * quantity;
+    }
+
+    static int computeBill(int price, int quantity, int coupon) {
+        return computeBill(price, quantity) - coupon;
+    }
 }
